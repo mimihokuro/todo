f29bef96cf4b93ee8ad6a9d3377e0873f2ace2b7
mimihokuro <keetasan7@gmail.com>
2022-11-03 23:13:02 +0900

    ⚡️ 未完了TODOに削除機能を実装

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -57,6 +57,18 @@ export default function Home() {
     [completedTodoList]
   );
 
+  const handleTodoCancel = useCallback(
+    (index) => {
+      setIncompletedTodoList((prevTodoList) => {
+        const afterDeleteTodoList = [...prevTodoList];
+        afterDeleteTodoList.splice(index, 1);
+        return afterDeleteTodoList;
+      });
+      return incompletedTodoList;
+    },
+    [incompletedTodoList]
+  );
+
   const handleTodoDelete = useCallback(
     (index) => {
       setCompletedTodoList((prevTodoList) => {
@@ -75,21 +87,27 @@ export default function Home() {
       <div>
         <button onClick={addNewTodo}>追加ボタン</button>
         <input value={todoText} onChange={inputTodoText} />
+        <h2>未完了</h2>
         <ul>
           {incompletedTodoList.map((todo, index) => {
             return (
               <li key={index}>
                 {todo}
                 <button onClick={() => handleTodoComplete(index)}>完了</button>
+                <button onClick={() => handleTodoCancel(index)}>削除</button>
               </li>
             );
           })}
         </ul>
+        <h2>完了</h2>
         <ul>
           {completedTodoList.map((todo, index) => {
             return (
               <li key={index}>
                 {todo}
+                <button onClick={() => handleTodoIncomplete(index)}>
+                  完了に戻す
+                </button>
                 <button onClick={() => handleTodoDelete(index)}>削除</button>
               </li>
             );
