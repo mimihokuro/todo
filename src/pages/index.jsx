e38ecb32f5a7878221797c1bd7bd5328cac6f550
mimihokuro <keetasan7@gmail.com>
2022-11-12 23:36:52 +0900

    💄 Tailwindcssでスタイリング

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -83,39 +83,93 @@ export default function Home() {
   };
 
   return (
-    <div>
-      <h1 className="text-3xl">TODO</h1>
-      <div>
-        <button onClick={handleAddNewTodo}>Add TODO</button>
-        <input value={todoText} onChange={handleInputTodoText} />
-        <h2>未完了</h2>
-        <ul>
-          {incompleteTodoList.map((todo, index) => {
-            return (
-              <li key={index}>
-                {todo}
-                <button onClick={() => handleCompleteTodo(index)}>完了</button>
-                <button onClick={() => handleCancelTodo(index)}>×</button>
-              </li>
-            );
-          })}
-        </ul>
-        <h2>完了</h2>
-        <ul>
-          {completeTodoList.map((todo, index) => {
-            return (
-              <li key={index}>
-                <label>
-                  {todo}
-                  <button onClick={() => handleIncompleteTodo(index)}>
-                    未完了にする
-                  </button>
-                </label>
-                <button onClick={() => handleDeleteTodo(index)}>×</button>
-              </li>
-            );
-          })}
-        </ul>
+    <div className="max-w-md m-auto p-8">
+      <h1 className="text-3xl font-bold">TODO List</h1>
+      <div className="mt-10">
+        <input
+          className="border-2 leading-4 p-2 mr-4 box-border"
+          value={todoText}
+          placeholder={"Register your TODO!"}
+          onChange={handleInputTodoText}
+        />
+        <button
+          className="p-2 text-white leading-4 rounded-md bg-gray-500"
+          onClick={handleAddNewTodo}
+        >
+          Add TODO
+        </button>
+        <div className="mt-10">
+          <h2 className="py-2 px-4 font-bold text-xl text-white bg-gray-700">
+            未完了
+          </h2>
+          <ul className="border-2">
+            {incompleteTodoList.length ? (
+              incompleteTodoList.map((todo, index) => {
+                return (
+                  <li
+                    key={index}
+                    className="flex justify-between align-middle border-t-2 m-auto py-2 px-4"
+                  >
+                    {todo}
+                    <div className="flex align-middle gap-2">
+                      <button
+                        className="p-2 text-white leading-4 rounded-md bg-gray-500"
+                        onClick={() => handleCompleteTodo(index)}
+                      >
+                        完了にする
+                      </button>
+                      <button
+                        className="px-3 py-1 text-white leading-4 rounded-md bg-gray-500"
+                        onClick={() => handleCancelTodo(index)}
+                      >
+                        ×
+                      </button>
+                    </div>
+                  </li>
+                );
+              })
+            ) : (
+              <span className="block p-2 text-gray-400">No TODOs...</span>
+            )}
+          </ul>
+        </div>
+        <div className="mt-5">
+          <h2 className="py-2 px-4 font-bold text-xl text-white bg-gray-700">
+            完了
+          </h2>
+          <ul className="border-2">
+            {completeTodoList.length ? (
+              completeTodoList.map((todo, index) => {
+                return (
+                  <li
+                    key={index}
+                    className="flex justify-between align-middle border-t-2 m-auto py-2 px-4"
+                  >
+                    {todo}
+                    <div className="flex align-middle gap-2">
+                      <button
+                        className="p-2 text-white leading-4 rounded-md bg-gray-500"
+                        onClick={() => handleIncompleteTodo(index)}
+                      >
+                        未完了にする
+                      </button>
+                      <button
+                        className="px-3 py-1 text-white leading-4 rounded-md bg-gray-500"
+                        onClick={() => handleDeleteTodo(index)}
+                      >
+                        ×
+                      </button>
+                    </div>
+                  </li>
+                );
+              })
+            ) : (
+              <span className="block p-2 text-gray-400">
+                All TODOs completed!
+              </span>
+            )}
+          </ul>
+        </div>
       </div>
     </div>
   );
